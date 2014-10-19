1d64c2af7d718e2bdaff9cf2abbae46922b6c464
RonRahaman <rahaman@cs.uchicago.edu>
2014-10-19 01:42:21 -0500

    new wait_on_children subroutine works

--- mach_pipeline.pl
+++ mach_pipeline.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 use strict;
 
 #############################################################################
@@ -8,12 +8,12 @@ use strict;
 my $wd = ".";               # the working directory; you may change this
 my $length = 2500;          # the "length" argument to ChunkChromosome; you may change this
 my $overlap = 500;          # the "overlap" argument to ChunkChromosome; you may change this
-my $maxNumThreads = 24;                 # the maximum number of process threads; you may change this
+my $maxCPU = 20;                 # the maximum number of process threads; you may change this
 my $currentNumThreads = 0;             # the current number of threads;  PLEASE DON'T CHANGE THIS
 my $pipelineLog = "mach_pipeline.log"; # Logfile for this pipeline script
 my $chunkChrLog = "chunk_chromosome.log"; # Logfile for ChunkChromosome
 
-my @child_pids;             # A list for the process ids (pids) of child threads
+my @child_pids = ();             # A list for the process ids (pids) of child threads
 
 #############################################################################
 #                            PART 1:  Running MaCH                          #
@@ -22,27 +22,24 @@ my @child_pids;             # A list for the process ids (pids) of child threads
 open (PIPELINE_LOG, ">", $pipelineLog) 
   or die "Unable to open $pipelineLog (the log file for this pipeline)";
 
-unlink $chunkChrLog or warn "Could not unlink $chunkChrLog: $!";
+unlink $chunkChrLog;
 
 print PIPELINE_LOG "Beginning MaCH pipeline (part 1)...\n";
 
-for my $chr ((22..22)) {
+for my $chr ((1..22, 'X', 'Y')) {
 
-  # Run ChunkChromosome for this chromosome, using the $length and $overlap
-  # variables.  
+  # Run ChunkChromosome for this chromosome
   system("ChunkChromosome -d chr${chr}.dat -n $length -o $overlap 2>&1 >> chunk_chromosome.log");
 
   # In the current directory, find all the files that were output from
   # ChunkChromosome.  Store the basename of these files in @chunks.
   my @chunks = get_chunks($chr);
 
-  my $numChunks = @chunks;
-  print PIPELINE_LOG "  In chr$chr, found $numChunks chunks: @chunks\n";
+  print PIPELINE_LOG "  In chr$chr, found ".scalar(@chunks)." chunks: @chunks\n";
 
   # Run mach on each chunk, using multiple processes
-  for my $chunk (@chunks) {
-    wait_on_children() if ($currentNumThreads >= $maxNumThreads);
-    $currentNumThreads++;
+  # for my $chunk (@chunks) {
+  for my $chunk (1..4) {
 
     # Fork execution into parent and child processes.  fork() returns the
     # process ID of the child process to the parent; and '0' to the child.
@@ -52,14 +49,17 @@ for my $chr ((22..22)) {
     # @child_pids
     if ($pid > 0) {
       push(@child_pids, $pid);
+      wait_on_children(\@child_pids) if (scalar(@child_pids) >= $maxCPU);
     }
 
     # If this is a child process, execute mach
     elsif ($pid == 0) {
-      my $command = "mach1 -d ${chunk} -p chr${chr}.ped --prefix ${chunk} ".
-          "--rounds 20 --states 200 --phase --sample 5 2>&1 > ${chunk}mach.log &";
+      my $command = "sleep 3; echo 'mach; chr${chr}, chunk${chunk}'";
+      # my $command = "mach1 -d ${chunk} -p chr${chr}.ped --prefix ${chunk} ".
+      #     "--rounds 20 --states 200 --phase --sample 5 2>&1 > ${chunk}mach.log &";
       print PIPELINE_LOG "  Executing '$command'\n";
       exec($command);
+      exit 1;
     }
 
     # If $pid < 0, an error has occured.
@@ -71,11 +71,9 @@ for my $chr ((22..22)) {
 }
 
 # Important!  Wait for all children to finish at the end.
-wait_on_children();
+wait_on_children(\@child_pids);
 
 print PIPELINE_LOG "...finished MaCH pipeline (part 1).\n";
-
-exit 0;
 
 #############################################################################
 #                            PART 2:  Running minimac                       #
@@ -83,7 +81,7 @@ exit 0;
 
 print PIPELINE_LOG "Beginning minimac pipeline (part 2)...\n";
 
-for my $chr ((22..22)) {
+for my $chr ((1..22, 'X', 'Y')) {
 
   # In the current directory, find all the files that were output from
   # ChunkChromosome.  Store the basename of these files in @chunks.
@@ -93,13 +91,12 @@ for my $chr ((22..22)) {
   print PIPELINE_LOG "  In chr$chr, found $numChunks chunks: @chunks\n";
 
   # Set the name for the vcf file. You may change this.
-  my $vcf = "~/hsdfiles/groups/Projects\GWAS\Bangladesh\1KG_phase3v5".
+  my $vcf = "~/hsdfiles/groups/Projects/GWAS/Bangladesh/1KG_phase3v5".
   "reduced.ALL.chr22.phase3_shapeit2_mvncall_integrated_v5.20130502.genotypes.vcf";
 
   # Run minimac on each chunk, using multiple processes
-  for my $chunk (@chunks) {
-    wait_on_children() if ($currentNumThreads + 4 > $maxNumThreads);
-    $currentNumThreads += 4;
+  # for my $chunk (@chunks) {
+  for my $chunk (1..4) {
 
     # Fork execution into parent and child processes.  fork() returns the
     # process ID of the child process to the parent; and '0' to the child.
@@ -109,18 +106,20 @@ for my $chr ((22..22)) {
     # @child_pids
     if ($pid > 0) {
       push(@child_pids, $pid);
+      wait_on_children(\@child_pids) if (scalar(@child_pids) * 4 > $maxCPU);
     }
 
     # If this is a child process, execute mach
     elsif ($pid == 0) {
-      my $command = "minimac-omp --cpus 4 --vcfReference --refHaps ${vcf}  ".
-        "--haps ${chunk}.gz --snps ${chunk}.snps --rounds 5 ".
-        "--states 200  --probs --autoClip autoChunk-chr${chr} --rs ".
-        "--snpAliases dbsnp134-merges.txt.gz  --prefix ${chunk}_minimac 2>&1 ".
-        "${chunk}_minimac.log &";
+      my $command = "sleep 3; echo 'minimac; chr${chr}, chunk${chunk}'";
+      # my $command = "minimac-omp --cpus 4 --vcfReference --refHaps ${vcf}  ".
+      #   "--haps ${chunk}.gz --snps ${chunk}.snps --rounds 5 ".
+      #   "--states 200  --probs --autoClip autoChunk-chr${chr} --rs ".
+      #   "--snpAliases dbsnp134-merges.txt.gz  --prefix ${chunk}_minimac 2>&1 ".
+      #   "${chunk}_minimac.log &";
       print PIPELINE_LOG "  Executing '$command'\n";
       exec($command);
-      exit 0;
+      exit 1;
     }
 
     # If $pid < 0, an error has occured.
@@ -132,7 +131,7 @@ for my $chr ((22..22)) {
 }
 
 # Important!  Wait for all children to finish at the end.
-wait_on_children();
+wait_on_children(\@child_pids);
 
 print PIPELINE_LOG "...finished minimac pipeline (part 2).\n";
 close(PIPELINE_LOG);
@@ -142,13 +141,14 @@ close(PIPELINE_LOG);
 #                       Subroutines definitions                             #
 #############################################################################
 
-# This subroutine waits on the child processes in @child_pids to finish.
+# Waits on a list of children to finish
+# Args: \@array, a ref to a list of child pids
 sub wait_on_children {
-  for my $pid (@child_pids) {
+  my $children = shift;
+  for my $pid (@$children) {
     waitpid($pid, 0) ;
   }
-  $currentNumThreads = 1;
-  @child_pids = ();
+  @$children = ();
 }
 
 # This subroutine gets the chunks from the working directory
